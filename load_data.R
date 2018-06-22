44e59a672e111c8b29303a5bb3aa10ca7c323466
Erin Reilly <erinmariereilly@gmail.com>
2018-06-22 10:52:26 -0400

    update to include the salinitykayak package

--- load_data.R
+++ load_data.R
@@ -1,11 +1,13 @@
+#If you don't have the salinity kayak package:
+devtools::install_github("marinereilly/salinitykayak")
+
 #load packages
 library(tidyverse)
 library(salinitykayak)
-library(dadjoke)
 library(sf)
 library(tmaptools)
 library(readxl)
-library(lubridate)
+
 
 #load salinity data
 salinity_master<-read_excel(path = "H:/0_HarrisLab/1_CURRENT PROJECT FOLDERS/Dominion/01_new_dominion/surveys/salinity_kayak/data/2018/2018_SalinityProfiles_DOMINION.xlsx",
@@ -15,12 +17,7 @@ a<-ggplot(salinity_master)+
   geom_histogram(aes(Waypoint), binwidth = 1)+theme_minimal()
 a
 
-#load GPX data
-sal1<-read_GPX(file = "H:/0_HarrisLab/1_CURRENT PROJECT FOLDERS/Dominion/01_new_dominion/surveys/01_GPS_waypoints/2018/salinity_02_14_2018.gpx", 
-               layers=c("waypoints"), as.sf = TRUE)
-sal2<-read_GPX(file = "H:/0_HarrisLab/1_CURRENT PROJECT FOLDERS/Dominion/01_new_dominion/surveys/01_GPS_waypoints/2018/salinity_02_28_2018.gpx",
-               layers=c("waypoints"), as.sf = TRUE)
+#run function; replace salinity_MM_DD_YYYY with the name of the gpx file (no .gpx)
+#this loads the data, formats the data, joins the data, and writes a new shapefile!
+salinity_wrangle("salinity_MM_DD_YYYY")
 
-#run function
-sal1<-salinity_format(sal1)
-sal2<-salinity_format(sal2)
